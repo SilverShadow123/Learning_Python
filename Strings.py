74233ba97dea371915c9877b57b317f61cad5c5b
SilverShadow123 <hasanmohasin852@gmail.com>
2025-10-13 15:50:28 +0600

    Initial commit

---
+++ Strings.py
@@ -0,0 +1,87 @@
+
+stg = 'the quick brown fox jumps over the lazy dog'
+stg2 = 'I am vengence I am batman'
+stg3 = 'I am the heavenly Demon'
+d1={}
+d2={1:'welcome',2:'goodbye',3:'and',4:'evening'}
+d3={'name':'Seaum', 'age':22, 'profession':'Python'}
+
+d5=({'q':[1,2,3,4,5],'z':[6,7,8,9,10]})
+d7 = {1: 'welcome', 2: 'goodbye', 3: 'and', 'rubbish': {'evening', 'night', 'welcome', 'to the jungle'}}
+# d7[1] = ('how', 'are', 'you')
+# d7[2] = 'go to hell'
+
+# print(d7.get('rubbish'))
+# d7.pop('rubbish')
+# d7.popitem()
+# print(d7.values())
+# keys = {'a', 'b', 'c'}
+# values = {1, 2, 3}
+# a=dict(zip(keys, values))
+# print(b)
+# print(a)
+
+s=set([1,3,3,2,2,3,4,5,6])
+s.add(5)
+print(s)
+s.remove(3)
+print(s)
+s.discard(4)
+print(s)
+s.pop()
+print(s)
+
+
+# print(d7)
+
+
+
+
+
+
+
+
+
+# stg = 'Tim said "I\'m busy today".'
+# stg1 = '''hey there'
+#         wel come to doom'''
+# stg2 = 'dragonballz'
+#
+# stg3 = 'welcome to the dragon island'
+#
+#
+# stg4 ="{} {}, {}".format(stg1, stg2, stg3)
+# print(stg4)
+
+
+
+
+# print(stg.upper())
+# print(stg.lower())
+# print(stg.find('s'))
+# print(stg.index('s'))
+
+
+
+# print(stg.split('s'))
+# x=stg.split('s')
+# y=stg.replace('s','ing')
+# z=stg.rpartition(" said ")
+# print(z)
+# stg4=stg1+stg2+stg3
+# print(stg4)
+
+
+
+
+# print(stg2[5:])
+# print(stg2[:5])
+# print(stg2[2:5])
+
+
+
+# for i in stg :
+#     print(i, end=" ")
+# print('\n')
+# print(stg[0:5])
+# print(stg2[5])
